42eb8f41fbf82ecd719c194cc296c9e0521eca7e
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-28 11:12:59 +0530

    Update and rename README.md to remove_vowels.py

---
+++ remove_vowels.py
@@ -0,0 +1,12 @@
+# remove_vowels
+def main():
+    pass
+    n=input()
+    inp=input().lower()
+    list_1=list(inp)
+    for i in inp:
+        if(i=="a" or i=="e" or i=="i" or i=="o" or i=="u"):
+            list_1.remove(i)
+    print(''.join(list_1[::-1]))
+if __name__ == '__main__':
+    main()
